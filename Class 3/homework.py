0e5be1b79150d559d2bef11e69e85a04cc80f497
mattmckofke <matt.mckofke@gmail.com>
2024-09-26 22:48:01 -0400

    finished-homework

---
+++ Class 3/homework.py
@@ -0,0 +1,13 @@
+## Homework
+# Write a script that:
+# - Declares a string variable `favorite_food`, an integer variable `quantity`, and a float variable `price`.
+# - Changes `favorite_food` to a different food item.
+# - Prints all variables before and after the change.
+
+favorite_food = "Pizza"
+quantity = 2
+price = 3.99
+print(favorite_food, quantity, price)
+
+favorite_food = "Pasta"
+print(favorite_food, quantity, price)
