a4e5d469b5bbce6906357f272b0bbe049ef67e03
mattmckofke <matt.mckofke@gmail.com>
2024-10-01 00:26:48 -0400

    class-4-materials

---
+++ Class 4/Operators-Homework.py
@@ -0,0 +1,10 @@
+## Homework
+# Write a Python script that:
+# - Calculates and prints the result of `(50 - 5*6) / 4`.
+# - Checks if `100` is not equal to `90` and prints the result.
+# - Uses `and`, `or`, and `not` to evaluate `((5 > 3) and (10 < 12)) or not(15 < 20)` and prints the outcome.
+
+x = (50 - 5*6) / 4
+print(x)
+print(100 != 90)
+print(((5 > 3) and (10 < 12)) or not(15 < 20))
