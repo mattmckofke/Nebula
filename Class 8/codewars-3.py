0f037880c92a1eb80cfa2e089809e7b01ba0fb24
mattmckofke <matt.mckofke@gmail.com>
2024-10-09 19:19:53 -0400

    updated-codewars-3

--- Class 8/codewars-3.py
+++ Class 8/codewars-3.py
@@ -4,4 +4,9 @@ def accum(s):
         result.append(s[i].upper() + s[i].lower() * i)
     return '-'.join(result)
 
-print(accum("abcd")) # "A-Bb-Ccc-Dddd"
+print("accum: " + accum("abcd"))
+
+def accum2(s):
+    return '-'.join([s[i].upper() + s[i].lower() * i for i in range(len(s))])
+
+print("accum2: " + accum2("abcd"))
